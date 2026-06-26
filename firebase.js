edbdf8712ddede04f05a8cf36914c291fe050b28
vangal <galkinivan773@gmail.com>
2026-06-26 20:50:45 +0400

    Set up Firebase and Firestore with global access
    Initialize Firebase app and Firestore database, and expose Firestore methods globally.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+  getFirestore, 
+  collection, 
+  addDoc, 
+  getDocs 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyACYzdq_7A4v9xoVqIgL7euA2UvfzfI0lI",
+  authDomain: "marks-store-13c0a.firebaseapp.com",
+  projectId: "marks-store-13c0a",
+  storageBucket: "marks-store-13c0a.firebasestorage.app",
+  messagingSenderId: "843845829558",
+  appId: "1:843845829558:web:e7717f00ae57d10f7ff717"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// делаем доступным глобально
+window.db = db;
+window.firestore = {
+  collection,
+  addDoc,
+  getDocs
+};
