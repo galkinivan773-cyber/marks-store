48af174181902fb8cf0ad93f6c01cc186f3aab82
vangal <galkinivan773@gmail.com>
2026-06-26 23:09:07 +0400

    Refactor Firebase initialization and Firestore access

--- firebase.js
+++ firebase.js
@@ -1,27 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { 
-  getFirestore, 
-  collection, 
-  addDoc, 
-  getDocs 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyACYzdq_7A4v9xoVqIgL7euA2UvfzfI0lI",
   authDomain: "marks-store-13c0a.firebaseapp.com",
   projectId: "marks-store-13c0a",
   storageBucket: "marks-store-13c0a.firebasestorage.app",
   messagingSenderId: "843845829558",
   appId: "1:843845829558:web:e7717f00ae57d10f7ff717"
-};
+});
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = firebase.firestore();
 
-// делаем доступным глобально
 window.db = db;
-window.firestore = {
-  collection,
-  addDoc,
-  getDocs
-};
+
+// 🔥 ДИАГНОСТИКА
+console.log("Firebase OK");
